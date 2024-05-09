91deba70c5f9e3b2b99afbe85d6933e1a5236def
solunska <a_solunska@outlook.com>
2024-05-09 15:38:33 +0200

    first commit

---
+++ EShopAdminApplication/EShopAdminApplication/Models/Concert.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace EShopAdminApplication.Models
+{
+    public class Concert
+    {
+        public Guid Id { get; set; }
+        [Required]
+        public string ConcertName { get; set; }
+        [Required]
+        public string ConcertDescription { get; set; }
+        [Required]
+        public string ConcertImage { get; set; }
+        [Required]
+        public double Rating { get; set; }
+    }
+}
---
+++ EShopAdminApplication/EShopAdminApplication/Models/Ticket.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System;
+
+namespace EShopAdminApplication.Models
+{
+    public class Ticket
+    {
+        public Guid Id { get; set; }
+        public Guid ConcertId { get; set; }
+        public Concert? Concert { get; set; }
+
+        [Required]
+        public double Price { get; set; }
+
+        [Required]
+        public double Rating { get; set; }
+    }
+}
